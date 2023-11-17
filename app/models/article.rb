b8d0043ddf4c918870ebc437812d2da46a092b0c
Ricardo Otero <oterosantos@gmail.com>
2023-11-17 11:18:23 +0000

    New cloudinary env variable

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,8 @@
 class Article < ApplicationRecord
   has_one_attached :banner
   # has_one_attached :icon
-  # has_one_attached :photo
+
+  # This would require us to permit "photos: []" in the controller and have the
+  # { multiple: true } in the f.input of the form
+  # has_many_attached :photos
 end
--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,6 +1,6 @@
 require 'cloudinary'
 
-Cloudinary.config_from_url("cloudinary://#{ENV['CLOUDINARY_API_KEY']}:#{ENV['CLOUDINARY_SECRET']}@#{ENV['CLOUDINARY_CLOUD_NAME']}")
+Cloudinary.config_from_url(ENV['CLOUDINARY_URL'])
 Cloudinary.config do |config|
   config.secure = true
 end
